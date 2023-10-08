62a7d6c022dea837e73471422a677edcf693140f
Chen-Rong-Zi <1398881912@qq.com>
2023-10-08 15:23:23 +0800

    fix wemeet pop window acting like monster in dwm

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -9,18 +9,19 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
-static const char *altbarcmd        = "~/.config/polybar/pwidgets/launch"; /* Alternate bar launch command */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=14";
+static const char *altbarcmd        = "/home/rongzi/.config/polybar/pwidgets/launch"; /* Alternate bar launch command */
+static const char *fonts[]          = { "Source Han Sans CN:size=10" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_blue[]        = "#3399c8";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeSel]  = { col_gray4, col_cyan,  col_blue  },
 };
 
 /* tagging */
@@ -31,10 +32,12 @@ static const Rule rules[] = {
      *  WM_CLASS(STRING) = instance, class
      *  WM_NAME(STRING) = title
      */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-    { NULL,       NULL,       "joshuto",  0,       1,           1  },
+    /* class          instance    title       tags mask     isfloating   monitor */
+    { "Gimp",         NULL,       NULL,       0,            1,           -1 },
+    { "Firefox",      NULL,       NULL,       1 << 8,       0,           -1 },
+    { NULL,           NULL,       "joshuto",  0,            1,           1  },
+    { NULL,           NULL,       "EmojiFloatWnd",  0,            1,           1  },
+    {"xfce4-notifyd", NULL,       NULL,       0,            1,           1  }
 };
 
 /* layout(s) */
@@ -62,17 +65,43 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
- static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-// static const char *dmenucmd[] = { "bash ~/.config/polybar/pwidgets/scripts/powermenu.sh", NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "alacrittyal", NULL };
+static const char *powerMenu[]  = { "/home/rongzi/.config/polybar/pwidgets/scripts/powermenu.sh", NULL };
+static const char *drun[]       = {"rofi", "-modi", "drun", "-show", "drun", "-config", "/home/rongzi/.config/rofi/main_menu.rasi", NULL};
+static const char *winMenu[]    = {"rofi", "-modi", "window", "-show", "window", "-config", "/home/rongzi/.config/rofi/main_menu.rasi", NULL};
+static const char *chrome[]     = {"google-chrome-stable", NULL};
+static const char *lock[]       = {"/home/rongzi/.config/scripts/lock", NULL};
+static const char *shot[]       = {"/home/rongzi/.config/scripts/shot", NULL};
+static const char *mouseShot[]  = {"/home/rongzi/.config/scripts/mouse_shot", NULL};
+static const char *picom[]      = {"/home/rongzi/.config/scripts/picom", NULL};
+static const char *myPicom[]    = {"/home/rongzi/.config/scripts/my_picom", NULL};
+static const char *blueman[]    = {"blueman-manager", NULL};
+static const char *btop[]       = {"/home/rongzi/.config/scripts/btop", NULL};
+static const char *joshuto[]    = {"/home/rongzi/.config/scripts/joshuto", NULL};
+static const char *clipboard[]  = {"/home/rongzi/.config/scripts/clipboard", NULL};
+static const char *pcmanfm[]    = {"pcmanfm", NULL};
 
-static const char *termcmd[]  = { "alacrittyal", NULL };
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
-    { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
     { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_d,      spawn,          {.v = drun } },
+    { MODKEY,                       XK_o,      spawn,          {.v = clipboard } },
+    { MODKEY,                       XK_t,      spawn,          {.v = pcmanfm } },
+    { MODKEY,                       XK_v,      togglebar,      {0} },
+    { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = winMenu } },
+    { MODKEY|ShiftMask,             XK_e,      spawn,          {.v = powerMenu } },
+    { MODKEY,                       XK_c,      spawn,          {.v = chrome } },
+    { MODKEY|ShiftMask,             XK_x,      spawn,          {.v = lock } },
+    { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = shot } },
+    { MODKEY|ShiftMask,             XK_a,      spawn,          {.v = mouseShot } },
+    { MODKEY,                       XK_n,      spawn,          {.v = picom } },
+    { MODKEY,                       XK_b,      spawn,          {.v = myPicom } },
+    { MODKEY|ShiftMask,             XK_b,      spawn,          {.v = blueman } },
+    { ControlMask|ShiftMask,        XK_Return, spawn,          {.v = btop } },
+    { ControlMask|ShiftMask,        XK_h,      spawn,          {.v = joshuto } },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
     { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -82,8 +111,8 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
     { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                       XK_e,      setlayout,      {.v = &layouts[0]} },
+    { MODKEY,                       XK_w,      setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_space,  setlayout,      {0} },
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -105,7 +134,6 @@ static const Key keys[] = {
     TAGKEYS(                        XK_7,                      6)
     TAGKEYS(                        XK_8,                      7)
     TAGKEYS(                        XK_9,                      8)
-    { MODKEY|ShiftMask,             XK_e,      quit,           {0} },
 };
 
 /* button definitions */
@@ -118,7 +146,7 @@ static const Button buttons[] = {
     { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
     { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
     { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+    { ClkClientWin,     MODKEY|ShiftMask,   Button1,        resizemouse,    {0} },
     { ClkTagBar,            0,              Button1,        view,           {0} },
     { ClkTagBar,            0,              Button3,        toggleview,     {0} },
     { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
@@ -133,4 +161,5 @@ static const char *ipcsockpath = "/tmp/dwm.sock"; static IPCCommand ipccommands[
   IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
   IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
 };
+
 
